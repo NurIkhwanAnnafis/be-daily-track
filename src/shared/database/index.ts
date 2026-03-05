0f57def7ac3a140d0ab61ac86b02ceeb43ba3db4
ikhwan <nur.ikhwan.annafis@gmail.com>
2026-03-05 13:29:37 +0700

    fix: disable SSL for pg pool on Render internal network

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -4,7 +4,8 @@ import { env } from "../../config/env"
 import * as schema from "./schema"
 
 const pool = new Pool({
-  connectionString: env.DATABASE_URL
+  connectionString: env.DATABASE_URL,
+  ssl: env.NODE_ENV === 'production' ? false : undefined,
 })
 
 export const db = drizzle(pool, { schema })
